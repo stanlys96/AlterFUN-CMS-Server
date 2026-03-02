3677e8ce7cc83efc54b26581be49f4e696e83e1a
stanlys96 <stanlyskwok@gmail.com>
2026-03-02 13:03:22 +0700

    wlao

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -484,7 +484,7 @@ export interface ApiCreatorApplicationCreatorApplication
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    email: Schema.Attribute.Email;
+    email: Schema.Attribute.Email & Schema.Attribute.Unique;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
@@ -583,7 +583,8 @@ export interface ApiRegisteredUserRegisteredUser
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    email: Schema.Attribute.Email;
+    did: Schema.Attribute.String;
+    email: Schema.Attribute.Email & Schema.Attribute.Unique;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
